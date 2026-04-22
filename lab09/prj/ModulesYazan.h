549db234e0f795c29acd914ffc2da67c89ad6e67
Kirill Yazan <ykirillyazan@gmail.com>
2026-04-22 20:43:47 +0300

    added header file

---
+++ lab09/prj/ModulesYazan.h
@@ -0,0 +1,17 @@
+#ifndef MODULES_H_INCLUDED
+#define MODULES_H_INCLUDED
+#include <string>
+
+// --- Задача 9.1 ---
+void windInfo(double v, int &b, std::string &desc);
+
+// --- Задача 9.2 ---
+double avgTempC(double arr[], int n);
+double toFahrenheit(double c);
+
+// --- Задача 9.3 ---
+int bitTask(unsigned int N);
+
+double s_calculation(double x, double y, double z);
+
+#endif
